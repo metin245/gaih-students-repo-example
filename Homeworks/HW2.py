327b223d59da0e1e1b97ae5e0944b9395dca78dd
metin245 <78621081+metin245@users.noreply.github.com>
2021-02-22 00:16:08 +0300

    Add files via upload

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,30 @@
-#Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+Student_Grade=[]
+Student_Name=[]
+Info={}
+Midterm_All=[]
+Final_All=[]
+Homework_All=[]
+ort=[]
+ort_2=[]
+ort_3=[]
+info={}
+sorted_avgDict={}
+import math
+for i in range(2):
+    student = input("Öğrencinin Adını ve Soyadını Aralarında Boşluk Olacak Şekilde Giriniz")
+    Student_Name.append(student)
+    Final=list(map(float, input("Öğrencinin Midterm notunu,Final Notunu ve Homework Notunu aralarında"
+                                " virgül olacak şekide giriniz Giriniz").split(',')))
+    Final_All.append(Final)
+info=dict(zip(Student_Name,Final_All))
+print(info)
+avgDict = {}
+for k,v in info.items():
+    # v is the list of grades for student k
+    avgDict[k] = sum(v)/ float(len(v))
+
+sorted_avgDict=dict(sorted(avgDict.items(),key=lambda item: item[1],reverse=True))
+print(sorted_avgDict)
+max_key = max(sorted_avgDict, key=sorted_avgDict.get)
+print("congrulation",max_key,"you are the best grade")
--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,24 @@
-#Explain your work
+def prime_first():
+    for i in range(500):
+        k = True
+        if i<=1:
+            k=False
+        for j in range(2, i):
+            if i%j == 0:
+                k = False
+        if k:
+           print(i)
 
-#Question 1
-for x in range(a):
-	print(a)
+
+
+def prime_second():
+    for i in range(500,1000):
+        k = True
+        for j in range(2, i):
+            if i%j == 0:
+                k = False
+        if k:
+           print(i)
+
+prime_first()
+prime_second()
